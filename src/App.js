3dcf332b8afbce7fc012b960202f48daafaa3543
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-26 11:15:46 -0700

    Fetch presidents and connect to the store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
+import './styles/CardContainer.css';
+import { getPresidents, setLoading, setError } from './actions';
+import { connect } from 'react-redux';
+import CardContainer from './containers/CardContainer';
 
 class App extends Component {
-  componentDidMount() {}
+  async componentDidMount() {
+    const { getPresidents, setLoading, setError } = this.props;
+    const response = await fetch('http://localhost:3001/api/v1/presidents');
+    const result = await response.json();
+    getPresidents(result);
+    setLoading(false);
+  }
 
   render() {
-    return <div className="App" />;
+    const { isLoading } = this.props;
+    return (
+      <div className="App">
+        <h1>Presidents and Assholes</h1>
+        {!isLoading && <CardContainer />}
+        {isLoading && <div>loading</div>}
+      </div>
+    );
   }
 }
 
-export default App;
+export const mapStateToProps = state => ({
+  presidents: state.presidents,
+  isLoading: state.isLoading,
+  error: state.hasErrored
+});
+
+export const mapDispatchToProps = dispatch => ({
+  getPresidents: presidents => dispatch(getPresidents(presidents)),
+  setError: error => dispatch(setError(error)),
+  setLoading: bool => dispatch(setLoading(bool))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
