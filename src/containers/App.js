9895957d50c5eba67479923285873d641102a55e
Hillary Stewart <hillarystewart4427@gmail.com>
2019-02-26 12:01:19 -0700

    Final commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import './styles/App.css';
-import './styles/CardContainer.css';
-import { getPresidents, setLoading, setError } from './actions';
+import '../styles/App.css';
+import '../styles/CardContainer.css';
+import { getPresidents, setLoading, setError } from '../actions';
 import { connect } from 'react-redux';
-import CardContainer from './containers/CardContainer';
+import CardContainer from './CardContainer';
 
-class App extends Component {
+export class App extends Component {
   async componentDidMount() {
     const { getPresidents, setLoading, setError } = this.props;
-    const response = await fetch('http://localhost:3001/api/v1/presidents');
-    const result = await response.json();
-    getPresidents(result);
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/presidents');
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const result = await response.json();
+      getPresidents(result);
+    } catch (error) {
+      setError('There was an error fetching the needed data, try again');
+    }
     setLoading(false);
   }
 
   render() {
-    const { isLoading } = this.props;
+    const { isLoading, hasErrored } = this.props;
     return (
       <div className="App">
         <h1>Presidents and Assholes</h1>
         {!isLoading && <CardContainer />}
-        {isLoading && <div>loading</div>}
+        {isLoading && <div>I am loading...</div>}
+        {hasErrored !== '' && <div>{hasErrored}</div>}
       </div>
     );
   }
@@ -29,7 +37,7 @@ class App extends Component {
 export const mapStateToProps = state => ({
   presidents: state.presidents,
   isLoading: state.isLoading,
-  error: state.hasErrored
+  hasErrored: state.hasErrored
 });
 
 export const mapDispatchToProps = dispatch => ({
